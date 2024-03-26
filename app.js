45fb3d774f9033d5ab76ce9288f86b872bc75eb3
Shehbaz-Niazi <163367421+Shehbaz-Niazi@users.noreply.github.com>
2024-03-25 18:02:49 -0700

    Todo-App
    : A todo list application written in TypeScript and Node.js. Perfect for beginners to learn basic backend development skills.
    
    Features:
    
    * Implements CRUD operations for todo items.
    * Includes TypeScript typings for improved code quality.
    * Hosted on GitHub for collaboration and contribution.

---
+++ app.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000);
+    });
+};
+async function chalk_Animation() {
+    let rainbow = chalkAnimation.rainbow(" <<<====>>> Let's Get Started <<<====>>>");
+    await sleep();
+    rainbow.stop();
+}
+await chalk_Animation();
+let todos = [];
+let loop = true;
+while (loop) {
+    let answers = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: chalk.blue.bold("Add Todos......"),
+        },
+        {
+            name: "Addmore",
+            type: "input",
+            message: "Do You Want To Add More Todo",
+            default: false,
+        },
+    ]);
+    let { todo, Addmore } = answers;
+    loop = Addmore;
+    if (todo) {
+        todos.push(todo);
+    }
+    else {
+        console.log("Enter A Valid Input.");
+    }
+}
+if (todos.length > 0) {
+    console.log(chalk.green.bold.underline("Your Todos List"));
+    todos.forEach((value) => {
+        console.log(value);
+    });
+}
+else {
+    console.log(chalk.red.bold("No Todos Found"));
+}
